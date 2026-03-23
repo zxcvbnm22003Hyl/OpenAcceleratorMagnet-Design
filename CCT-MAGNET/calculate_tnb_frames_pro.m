b52fe0bd67ee8a7f55d5681d795eb69e72b52365
河鱼鳞 <13946025878@163.com>
2026-03-23 15:06:04 +0800

    Delete CCT-MAGNET/calculate_tnb_frames_pro.m

--- CCT-MAGNET/calculate_tnb_frames_pro.m
+++
@@ -1,8 +0,0 @@
-﻿function full_path_data = calculate_tnb_frames_pro(path_xyz)
-% Compatibility wrapper that delegates to shared RMF TNB implementation.
-
-this_dir = fileparts(mfilename('fullpath'));
-src_dir = fullfile(this_dir, '..', '..', '..');
-addpath(src_dir);
-full_path_data = compute_tnb_rmf(path_xyz);
-end
